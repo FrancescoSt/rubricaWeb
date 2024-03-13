ba168573784bc9ff22cf467d2eb4ecf7b86b61c4
Francesco Adrian Stoppa <127590071+FrancescoSt@users.noreply.github.com>
2024-03-13 10:48:18 +0100

    Add files via upload

---
+++ rubricaWeb/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using rubricaWeb.Models;
+
+namespace rubricaWeb.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        //List<string> Lista = new();
+        //List.Add("Maurizio");
+        //List.Add("Marco");
+        //List.Add("Francesco");
+
+        return View( );
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
